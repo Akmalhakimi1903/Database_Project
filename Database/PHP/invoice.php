f3fad348d4a1c8b2c351c92a79772e4e76b96a3d
Akmalhakimi1903 <53884345+Akmalhakimi1903@users.noreply.github.com>
2025-05-22 23:39:22 +0800

    invoice.php

--- Database/PHP/invoice.php
+++ Database/PHP/invoice.php
@@ -1,9 +1,9 @@
 <?php
 //database
-$host = 'localhost'; 
-$dbname = 'highbridgecreative'; 
+$host = 'localhost';
+$dbname = 'highbridgecreative';
 $username = 'root';
-$password = ''; 
+$password = '';
 
 //connection
 $conn = new mysqli($host, $username, $password, $dbname);
@@ -16,20 +16,23 @@ if ($conn->connect_error) {
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Invoice</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href= "../CSS/style.css">
-    </head>
-    <body>
-        <?php include 'sidebar.html'; ?>
-        <!-- Main Content -->
-        <div class="content" id="content">
-            <input class="icon" type="image" src="../PNG/Icon/sidebarIcon.png" onclick="toggleSidebar()"/>
-            <div>
 
-            </div>
+<head>
+    <title>Invoice</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../CSS/style.css">
+</head>
+
+<body>
+    <?php include 'sidebar.html'; ?>
+    <!-- Main Content -->
+    <div class="content" id="content">
+        <input class="icon" type="image" src="../PNG/Icon/sidebarIcon.png" onclick="toggleSidebar()" />
+        <div class="scrolling-text">
+            <span>𝓗𝓲𝓰𝓱 𝓑𝓻𝓲𝓭𝓰𝓮 𝓒𝓻𝓮𝓪𝓽𝓲𝓿𝓮</span>
         </div>
-        <script src="../JS/script.js"></script>
-    </body>
+    </div>
+    <script src="../JS/script.js"></script>
+</body>
+
 </html>
